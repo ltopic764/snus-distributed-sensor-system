96122a364d1ad06f4d99220c1a2cb926507a24b8
ltopic764 <ltopic764@gmail.com>
2026-06-16 18:52:03 +0200

    SensorClient

---
+++ SNUSSensorSystem/SNUSSensorSystem.SensorClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SNUSSensorSystem.SensorClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
